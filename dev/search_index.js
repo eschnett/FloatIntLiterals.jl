4279316f4c3d975f8100b8e76e9dea3c7ce24d37
Documenter.jl <documenter@juliadocs.github.io>
2024-06-11 23:18:24 +0000

    build based on caf6cc8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FloatIntLiterals.jl","page":"FloatIntLiterals.jl","title":"FloatIntLiterals.jl","text":"","category":"section"},{"location":"","page":"FloatIntLiterals.jl","title":"FloatIntLiterals.jl","text":"Modules = [FloatIntLiterals]","category":"page"}]
+}
